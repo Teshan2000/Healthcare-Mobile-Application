faaf80d79dde26244d8ab0aeb07919e8039a1db9
Teshan2000 <teshan_wijewardhana@outlook.com>
2023-06-15 09:08:53 +0530

    Configurations
    Config files were added to the system to initialize configurations.

--- healthcare_management_system/lib/main.dart
+++ healthcare_management_system/lib/main.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  int currentPage = 0;
-
+  var onPressed;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -31,6 +28,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int currentPage = 0;
   int _counter = 0;
 
   void _incrementCounter() {
@@ -43,29 +41,21 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        centerTitle: true,
+        title: Text('Put Your Topic Here'),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu),
         ),
       ),
+      body: Center(),
       bottomNavigationBar: NavigationBar(
+        backgroundColor: Colors.blue,
         destinations: const [
-          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-          NavigationDestination(icon: Icon(Icons.message), label: "Messages"),
-          NavigationDestination(
-              icon: Icon(Icons.calendar_month), label: "Schedule"),
-          NavigationDestination(icon: Icon(Icons.settings), label: "Settings"),
+          NavigationDestination(icon: Icon(Icons.home, color: Colors.white), label: "Home"),
+          NavigationDestination(icon: Icon(Icons.message, color: Colors.blueGrey), label: "Messages"),
+          NavigationDestination(icon: Icon(Icons.calendar_month, color: Colors.blueGrey), label: "Schedule"),
+          NavigationDestination(icon: Icon(Icons.settings, color: Colors.blueGrey), label: "Settings"),
         ],
         onDestinationSelected: (int index) {
           setState(() {
@@ -73,11 +63,6 @@ class _MyHomePageState extends State<MyHomePage> {
           });
         },
         selectedIndex: currentPage,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
---
+++ healthcare_management_system/lib/utils/config.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class config {
+  static MediaQueryData? mediaQueryData;
+  static double? screenWidth;
+  static double? screenHeight;
+
+  void init(BuildContext context) {
+    mediaQueryData = MediaQuery.of(context);
+    screenWidth = mediaQueryData!.size.width;
+    screenHeight = mediaQueryData!.size.height;
+  }
+
+  static get widthSize {
+    return screenWidth;
+  }
+
+  static get heightSize {
+    return screenHeight;
+  }
+
+  static const spaceSmall = SizedBox(
+    height: 25,
+  );
+
+  static final spaceMedium = SizedBox(
+    height: screenHeight! * 0.05,
+  );
+
+  static final spaceLarge = SizedBox(
+    height: screenHeight! * 0.08,
+  );
+
+  static const outlinedBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+  );
+
+  static const focusBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(8)),
+      borderSide: BorderSide(
+        color: Colors.blue,
+      ));
+
+  static const errorBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(8)),
+      borderSide: BorderSide(
+        color: Colors.red,
+      ));
+
+  static const primaryColor = Colors.blue;
+}
