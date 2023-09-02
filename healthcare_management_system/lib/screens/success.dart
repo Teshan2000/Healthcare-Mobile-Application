be867ef0b2fa9a0813faf5657e7b036055ae350c
Teshan2000 <teshan_wijewardhana@outlook.com>
2023-09-02 22:37:16 +0530

    Success Page
    Success page was completely added to the system.

---
+++ healthcare_management_system/lib/screens/success.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import '../components/button.dart';
+
+class Success extends StatelessWidget {
+  const Success({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: Lottie.asset('Assets/success.json'),
+            ),
+            Container(
+              width: double.infinity,
+              alignment: Alignment.center,
+              child: const Text(
+                'Successfully Booked',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+              child: Button(
+                width: double.infinity,
+                title: 'Back to Home Page',
+                onPressed: () => Navigator.of(context).pushNamed('home'),
+                disable: false,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
