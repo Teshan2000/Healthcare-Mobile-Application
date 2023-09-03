6d692479cd70f56344525cc7e74a60740a7bebbf
Teshan2000 <teshan_wijewardhana@outlook.com>
2023-09-03 23:46:03 +0530

    Login Form
    Login Form component was completely added to the system.

--- healthcare_management_system/lib/components/loginForm.dart
+++ healthcare_management_system/lib/components/loginForm.dart
@@ -1,18 +1,95 @@
+
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../main.dart';
+import '../utils/config.dart';
+import 'button.dart';
 
 class LoginForm extends StatefulWidget {
   const LoginForm({Key? key}) : super(key: key);
 
   @override
-  State<LoginForm> createState() => _LoginFormState();
+  State<LoginForm> createState() => LoginFormState();
 }
 
-class _LoginFormState extends State<LoginForm> {
+class LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+  bool obsecurePass = true;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("LoginForm Page"),
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
+            cursorColor: Config.primaryColor,
+            decoration: const InputDecoration(
+              hintText: 'Email Address',
+              labelText: 'Email',
+              alignLabelWithHint: true,
+              prefixIcon: Icon(Icons.email_outlined),
+              prefixIconColor: Config.primaryColor,
+            ),
+          ),
+          Config.spaceSmall,
+          TextFormField(
+            controller: _passController,
+            keyboardType: TextInputType.visiblePassword,
+            cursorColor: Config.primaryColor,
+            obscureText: obsecurePass,
+            decoration: InputDecoration(
+                hintText: 'Password',
+                labelText: 'Password',
+                alignLabelWithHint: true,
+                prefixIcon: const Icon(Icons.lock_outline),
+                prefixIconColor: Config.primaryColor,
+                suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        obsecurePass = !obsecurePass;
+                      });
+                    },
+                    icon: obsecurePass
+                        ? const Icon(
+                            Icons.visibility_off_outlined,
+                            color: Colors.black38,
+                          )
+                        : const Icon(
+                            Icons.visibility_outlined,
+                            color: Config.primaryColor,
+                          ))),
+          ),
+          Config.spaceSmall,
+          // Consumer<AuthModel>(
+          //   builder: (context, auth, child) {
+          Button(
+            width: double.infinity,
+            title: 'Sign In',
+            onPressed: () async {
+                  // final token = await DioProvider()
+                  //     .getToken(_emailController.text, _passController.text);
+
+                  // if(token) {
+                  //   auth.loginSuccess();
+                  //   MyApp.navigatorKey.currentState!.pushNamed('login');
+                  // }
+                  //Navigator.of(context).pushNamed("login");
+            MyApp.navigatorKey.currentState!.pushNamed('login');
+            },
+            disable: false,
+          ),
+              
+          
+        
+        ],
       ),
     );
   }
